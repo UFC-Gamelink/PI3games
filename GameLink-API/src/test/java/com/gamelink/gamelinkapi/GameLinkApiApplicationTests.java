665062ff8179d96245c0aef14d37f23745c43ee6
victor <martinsvictor6781@gmail.com>
2023-09-04 10:59:24 -0300

    feat(backend): configuração da api
    criação do projeto springboot, configuração do banco de dados, adição das dependências

---
+++ GameLink-API/src/test/java/com/gamelink/gamelinkapi/GameLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelink.gamelinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLinkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
