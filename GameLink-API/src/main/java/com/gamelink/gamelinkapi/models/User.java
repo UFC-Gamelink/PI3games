0f0ec659bccb0fc03b8490cef316ace47831fcc9
victor <martinsvictor6781@gmail.com>
2023-09-09 10:54:41 -0300

    test(backend): testes de serviços para registro de usuário

--- GameLink-API/src/main/java/com/gamelink/gamelinkapi/models/User.java
+++ GameLink-API/src/main/java/com/gamelink/gamelinkapi/models/User.java
@@ -48,7 +48,7 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return email;
+        return username;
     }
 
     @Override
--- GameLink-API/src/test/java/com/gamelink/gamelinkapi/integration/AuthenticationControllerIT.java
+++ GameLink-API/src/test/java/com/gamelink/gamelinkapi/integration/AuthenticationControllerIT.java
@@ -13,12 +13,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class AuthenticationControllerIT {
     @Autowired
     private TestRestTemplate testRestTemplate;
