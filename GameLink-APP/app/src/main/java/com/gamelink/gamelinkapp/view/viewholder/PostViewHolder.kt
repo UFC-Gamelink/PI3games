a21f6f821a4caa2b08f5841a136cd74993e227da
Herik Cosmo <herikcosmo.m@gmail.com>
2023-11-15 18:08:03 -0300

    feat(app): botão de curtir em posts

--- GameLink-APP/app/src/main/java/com/gamelink/gamelinkapp/view/viewholder/PostViewHolder.kt
+++ GameLink-APP/app/src/main/java/com/gamelink/gamelinkapp/view/viewholder/PostViewHolder.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.gamelink.gamelinkapp.R
 import com.gamelink.gamelinkapp.databinding.RowPostsListBinding
 import com.gamelink.gamelinkapp.service.constants.GameLinkConstants
 import com.gamelink.gamelinkapp.service.listener.PostListener
@@ -16,6 +17,7 @@ class PostViewHolder(private val itemBinding: RowPostsListBinding, val listener:
     private val securityPreferences = SecurityPreferences(itemBinding.root.context)
 
     fun bindData(post: PostProfileModel) {
+        var liked = false
         itemBinding.textPost.text = post.post.post
 
         if(post.post.postImagePath != null) {
@@ -32,6 +34,16 @@ class PostViewHolder(private val itemBinding: RowPostsListBinding, val listener:
 
         if(post.post.userId != userId) {
             itemBinding.icDotMenu.visibility = View.GONE
+        }
+
+        itemBinding.imageLike.setOnClickListener {
+            liked = if(!liked) {
+                itemBinding.imageLike.setImageResource(R.drawable.ic_liked)
+                true
+            } else {
+                itemBinding.imageLike.setImageResource(R.drawable.ic_like)
+                false
+            }
         }
 
         itemBinding.icDotMenu.setOnClickListener {
