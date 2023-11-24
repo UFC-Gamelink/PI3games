541de4377771b33fe9cec33eaa6f78377d986b41
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2023-11-24 14:34:07 -0300

    feat(backend): adição da lista de posts na resposta da comunidade

--- GameLink-API/src/main/java/com/gamelink/gamelinkapi/mappers/CommunityMapper.java
+++ GameLink-API/src/main/java/com/gamelink/gamelinkapi/mappers/CommunityMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(uses = PostMapper.class)
 public interface CommunityMapper {
     CommunityMapper INSTANCE = Mappers.getMapper(CommunityMapper.class);
 
