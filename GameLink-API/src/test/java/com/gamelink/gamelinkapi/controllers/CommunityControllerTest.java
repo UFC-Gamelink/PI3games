fa454e53c3499d7063724566213dc4fcccc17ad1
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2023-11-28 10:10:59 -0300

    test(backend): testes para communityController

--- GameLink-API/src/test/java/com/gamelink/gamelinkapi/controllers/CommunityControllerTest.java
+++ GameLink-API/src/test/java/com/gamelink/gamelinkapi/controllers/CommunityControllerTest.java
@@ -3,16 +3,20 @@ package com.gamelink.gamelinkapi.controllers;
 import com.gamelink.gamelinkapi.dtos.requests.communities.CommunityRequest;
 import com.gamelink.gamelinkapi.dtos.requests.posts.PostRequest;
 import com.gamelink.gamelinkapi.dtos.responses.communities.CommunitiesGeneralResponse;
+import com.gamelink.gamelinkapi.dtos.responses.communities.CommunityResponse;
 import com.gamelink.gamelinkapi.dtos.responses.communities.PostCommunityResponse;
 import com.gamelink.gamelinkapi.services.communities.CommunityService;
 import com.gamelink.gamelinkapi.utils.creators.CommunityRequestCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -112,5 +116,61 @@ public class CommunityControllerTest {
         verify(service, times(1))
                 .addPost(communityId, new PostRequest(postText, null));
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("postBanner should execute createBanner in CommunityService and return a Created status when success")
+    void postBannerSuccess() {
+        var id = UUID.randomUUID();
+        MultipartFile multipartFile = Mockito.mock(MultipartFile.class);
+        var response = createCommunityResponse();
+
+        when(service.createBanner(id, multipartFile)).thenReturn(response);
+
+        ResponseEntity<CommunityResponse> responseReceived = controller.postBanner(id, multipartFile);
+
+        verify(service, times(1)).createBanner(id, multipartFile);
+        assertEquals(HttpStatus.CREATED, responseReceived.getStatusCode());
+        assertNotNull(responseReceived);
+        assertEquals(response, responseReceived.getBody());
+    }
+
+
+    @Test
+    @DisplayName("GetById should execute getCommunity in CommunityService and return a Ok status when success")
+    void getByIdSuccess() {
+        var id = UUID.randomUUID();
+        var validResponse = createCommunityResponse();
+        when(service.getCommunity(id)).thenReturn(validResponse);
+
+        ResponseEntity<CommunityResponse> response = controller.getById(id);
+
+        verify(service, times(1)).getCommunity(id);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(validResponse, response.getBody());
+    }
+
+    @Test
+    @DisplayName("GetMy should execute getMyCommunities in CommunityService and return a Ok status when success")
+    void getMySuccess() {
+        var validResponse = List.of(createCommunityGeneralResponse());
+        when(service.getMyCommunities()).thenReturn(validResponse);
+
+        ResponseEntity<List<CommunitiesGeneralResponse>> response = controller.getMy();
+
+        verify(service, times(1)).getMyCommunities();
+        assertNotNull(response.getBody());
+        assertEquals(1, response.getBody().size());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(validResponse.get(0), response.getBody().get(0));
+    }
+
+    private static CommunityResponse createCommunityResponse() {
+        return new CommunityResponse(UUID.randomUUID(), "name", "description", "url", "owner", UUID.randomUUID(), List.of());
+    }
+
+    private static CommunitiesGeneralResponse createCommunityGeneralResponse() {
+        return new CommunitiesGeneralResponse(UUID.randomUUID(), "name", "description", "url", "owner", UUID.randomUUID());
     }
 }
